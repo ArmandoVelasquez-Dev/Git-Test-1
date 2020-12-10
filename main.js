3741ae03ff4f890506e615ccb11aef7da5ff8210
Mario Ramírez <mario11842@hotmail.com>
2020-12-10 16:53:44 -0600

    Modificación de algunos aspectos e implementación de función en JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 document.write("Hi, from Javascript")
+document.write("<br> <br>")
+
+//Función con parámetro para saludar
+// let nombre = prompt(" Ingrese su nombre  ");
+// function saludar(name){
+//     let frase = `¡Hola ${name}!, ¿cómo estás?`;
+//     document.write(frase);
+// }
+// saludar(nombre);
+
+//Función flecha
+let nombre = prompt(" Ingrese su nombre  ");
+const saludar = (name)=>{
+    let frase = `¡Hola ${name}!, ¿cómo estás?`;
+    document.write(frase);
+}
+saludar(nombre);
